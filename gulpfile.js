f8e6d47347ab88da7ecb2bd846594971fe2eb1d1
Rafael Dal Forno <rafa_fln15@hotmail.com>
2024-12-04 22:18:14 -0300

    finalizando o hero do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -17,5 +17,5 @@ function images(){
 exports.default = gulp.parallel(styles, images);
 
 exports.watch = function(){
-  gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+  gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
 }
